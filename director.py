4b5cf01314ad37064f8608ae1eb66660c8313a11
Yuting Chen <yuting@YutingdeMac-mini.local>
2025-09-14 18:34:35 +0800

    director.py

--- director.py
+++ director.py
@@ -7,7 +7,7 @@ class StoryDirector:
         # 定义角色名称与剧情阶段
         self.character_names = ["埃勒里·奎因","玛尔博","赛布尔","阿祖尔"]
         self.contexts = {}
-        self.story_phases = ["与玛尔博的对话", "获取第一轮线索" ,"与赛布尔的对话", "获取第二轮线索", "与阿祖尔的对话", "获取第三轮线索", "推理凶手"]
+        self.story_phases = ["初来乍到"]
         self.phase_index = 0
 
     async def initialize_characters(self):
@@ -21,7 +21,7 @@ class StoryDirector:
 
     async def run_story_loop(self):
         print("\n【系统】剧情演化开始\n")
-        for i in range(7):
+        for i in range(len(self.story_phases)):
             phase = self.story_phases[self.phase_index]
             print(f"\n———第{i+1}轮 · 剧情阶段【{phase}】———")
             for name in self.character_names:
